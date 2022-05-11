ab8e840590ac979db54a548b011fca682e795fa3
MADARAPU SATHWIKA <72800563+Sathwika-02@users.noreply.github.com>
2022-05-11 07:24:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import Restaurant from './component/Basics/Restaurant'
+//import useState from './component/Hooks/useState'
+//import Todo from './component/todo/todo';
+import Temp from './component/weather/temp';
+const App = () => {
+  return (
+   <Temp/>
+  )
+}
+
+export default App;
